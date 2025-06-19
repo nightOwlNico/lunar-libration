6f96a9d389cd58ff5b0b4de00144a5590c832d77
Ñîçø <nightOwlNico@gmail.com>
2025-06-19 16:24:10 -0600

    updatig renderer to be responsive & handle hd-dpi displays

--- src/main.ts
+++ src/main.ts
@@ -44,8 +44,27 @@ function main() {
     makeInstance(geometry, 0xaa8844, 2),
   ];
 
+  function resizeRendererToDisplaySize(renderer) {
+    const canvas = renderer.domElement;
+    const pixelRatio = window.devicePixelRatio;
+    const width = Math.floor(canvas.clientWidth * pixelRatio);
+    const height = Math.floor(canvas.clientHeight * pixelRatio);
+    const needResize = canvas.width !== width || canvas.height !== height;
+    if (needResize) {
+      renderer.setSize(width, height, false);
+    }
+
+    return needResize;
+  }
+
   function render(time) {
     time *= 0.001;
+
+    if (resizeRendererToDisplaySize(renderer)) {
+      const canvas = renderer.domElement;
+      camera.aspect = canvas.clientWidth / canvas.clientHeight;
+      camera.updateProjectionMatrix();
+    }
 
     cubes.forEach((cube, ndx) => {
       const speed = 1 + ndx * 0.1;
